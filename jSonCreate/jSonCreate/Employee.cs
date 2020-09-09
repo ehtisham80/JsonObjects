bd199a57e4760b6a31abd90da34d092b33d8814b
Ehtisham Zaheer <ehtisham_zaheer@hotmail.com>
2020-09-09 11:55:53 +0200

    jSonCreate

---
+++ jSonCreate/jSonCreate/Employee.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jSonCreate
+{
+    class Employee
+    {
+        public string FirstName = "Ehtisham";
+        public string LastName = "Zaheer";
+        public int EmployeeID = 1509;
+        public string Designation = "Director";
+
+    }
+
+}
